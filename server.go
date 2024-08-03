138009d5f08911a8ac8eb7311238141f2055aaae
PaoloModica <modica.paolowalter@gmail.com>
2024-08-03 12:53:50 +0200

    add: GET /healthcheck route added to backend server

--- server.go
+++ server.go
@@ -1,9 +1,14 @@
 package load_balancer
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type HealthcheckStatus struct {
+	Status string
+}
 
 type BasicServer struct {
 	http.Handler
@@ -12,6 +17,7 @@ type BasicServer struct {
 func NewServer() *BasicServer {
 	router := http.NewServeMux()
 	router.Handle("/", http.HandlerFunc(homeRouteHandler))
+	router.Handle("/healthcheck", http.HandlerFunc(healthcheckRouteHandler))
 
 	server := BasicServer{router}
 
@@ -26,4 +32,12 @@ func homeRouteHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Accept: %v\n", r.Header.Get("Accept-Encoding"))
 
 	fmt.Fprintf(w, "hello %v", r.Host)
+}
+
+func healthcheckRouteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	status := HealthcheckStatus{"READY"}
+	json.NewEncoder(w).Encode(status)
 }
